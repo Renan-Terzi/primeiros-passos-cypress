77ff141378e32ee3cea6d9222a1cdb5b72d15795
Renan-Terzi <renan.terzi@gmail.com>
2025-02-12 06:38:05 -0300

    add randon values on user spec with chanceJS

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import DashboardPage from '../pages/dashboardPage.js'
 import MenuPage from '../pages/menuPage.js'
 import InfoPage from '../pages/infoPage.js'
 
+const Chance = require('chance')
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -20,8 +23,8 @@ describe('Orange HRM Tests', () => {
 
     menuPage.accessMyInfo()
 
-    infoPage.fillPersonalDetails('First Name', 'Last Name')
-    infoPage.fillEmployeeDetails('Employ ID', "Other ID", 'Drivers Licence', '2026-08-25')
+    infoPage.fillPersonalDetails(chance.first(), chance.last())
+    infoPage.fillEmployeeDetails(chance.bb_pin(), chance.bb_pin(), chance.bb_pin(), '2026-08-25')
     infoPage.fillStatus('1990-05-15')
     infoPage.saveForm()
 
